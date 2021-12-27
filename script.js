c9a3100a2d9468c82c92643a6f1bff6269db6888
Ilya <toupitsyn@gmail.com>
2021-12-27 17:19:14 +0300

    Release 1

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let student = {};
-student.firstName = "Petr";
-student.lastName = "Perviy";
+
+function getStudent(firstName, lastName) {
+  let student = {};
+  student.firstName = firstName;
+  student.lastName = lastName;
+  return student
+}
+
+
+let student = getStudent("Petr", "Perviy");
 student.firstName = "Petya";
 delete student.firstName;
 
 console.log(student);
+
+let group = [];
+group.push(student);
+group.push( getStudent("Ilshat", "Baburov") );
+group.push( getStudent("Vsevolod", "Pupyrkin") );
+
+console.log(group);
+
